2227c486c278e393b5b89dc59cb529dc668f9f5a
Anthony Miyoro <amiyoro@gmail.com>
2022-10-14 23:07:40 +0300

    Tweet scraping connected to DB

--- twitter_sraper.py
+++ twitter_sraper.py
@@ -7,20 +7,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-hashtag_string  = "#stopthesteal Trump"
-start_date = "since:2019-01-30"
-
 # Creating list to append tweet data to
 tweets_list2 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper(hashtag_string + ' ' + start_date).get_items()):
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper(os.environ.get('TWITTER_SEARCH_STRING') + ' ' + os.environ.get('TWITTER_SEARCH_FROM_DATE')).get_items()):
     # pprint(tweet)
     if i>int(os.environ.get('NUMBER_OF_TWEETS_TO_DOWNLOAD')):
         break
     tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.username])
 
-print ("i", i)   
+print ( i, " tweets collecgted.")   
 # Creating a dataframe from the tweets list above
 tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 
